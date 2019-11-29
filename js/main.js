3ca8db6788b78dd012827b621c9c014095d3f8b6
Tom Madsen <48400765+IntrepidTom@users.noreply.github.com>
2019-11-30 09:44:13 +1000

    Ready for submission
    Fixed validation errors.
    To load user data (login or logout), click the top right of the screen on the login text, or user profile image.

--- js/main.js
+++ js/main.js
@@ -11,29 +11,101 @@ let membersList = document.querySelector('.member-list');
 let recentList = document.querySelector('.recent-list');
 let notification = document.getElementById('notification');
 let noticeDiv = document.createElement('div');
+let trafficList = document.querySelector('.traffic-list');
 let loggedUser = {};
 loggedUser.viewedNotices = false;
 
 // Get a random for the top user profile and extra members to search for...
-
 fetch('https://randomuser.me/api/?results=15')
-    .then(
-        function (response) {
-            if (response.status == 200) {response.json().then(data => {
-                localStorage.setItem('randomUsers', JSON.stringify(data.results)); //in this case, the objects are wrapped in 'results' - saving locally to use outside of fetch scope.    
-                }); 
-            } else {
-                console.log('Looks like there was a problem. Status Code: ' +
-                    response.status);
-                return;
-            }
-        } 
-    )
-    .catch(function (err) {
-        console.log('Fetch Error :-S', err);
-});
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        localStorage.setItem('randomUsers', JSON.stringify(data.results)); // Work with JSON data here
+    })
+    .catch(err => {
+            console.log('Fetch Error :-S', err);
+    });
+
 let randomUsers = JSON.parse(localStorage.getItem('randomUsers'));  //turning our saved string to a usable object
-console.log(randomUsers);
+const members = [{
+        name: {
+            first: 'Victoria',
+            last: 'Chambers'
+        },
+        email: 'victoria.chambers80@example.com',
+        spec: {
+            method: "commented on",
+            medium: "YourApp's SEO Tips",
+            time: "4 hours ago"
+        },
+        picture: {
+            thumbnail: "images/member-1.jpg"
+        },
+        joindate: "09/30/2019"
+    },
+    {
+        name: {
+            first: 'Dale',
+            last: 'Byrd'
+        },
+        email: 'dale.byrd52@example.com',
+        spec: {
+            method: "liked the post",
+            medium: "Facebook's changes for 2016",
+            time: "5 hours ago"
+        },
+        picture: {
+            thumbnail: "images/member-2.jpg"
+        },
+        joindate: "11/05/2015"
+    },
+    {
+        name: {
+            first: 'Dawn',
+            last: 'Wood'
+        },
+        email: 'dawn.wood16@example.com',
+        spec: {
+            method: "commented on",
+            medium: "Facebook's changes for 2016",
+            time: "5 hours ago"
+        },
+        picture: {
+            thumbnail: "images/member-3.jpg"
+        },
+        joindate: "01/26/1901"
+    },
+    {
+        name: {
+            first: 'Dan',
+            last: 'Oliver'
+        },
+        email: 'dan.oliver52@example.com',
+        spec: {
+            method: "posted to",
+            medium: "YourApp's SEO Tips",
+            time: "1 day ago"
+        },
+        picture: {
+            thumbnail: "images/member-4.jpg"
+        },
+        joindate: "04/25/1939"
+    }
+];
+
+let userList = members.concat(randomUsers);
+try {
+    for (let i = 0; i < userList.length; i++) {
+        let user = userList[i];
+        user.name.full = user.name.first + " " + user.name.last;
+        user.name.bridged = user.name.first + user.name.last;
+    }
+} catch (e) {
+    console.log(e);
+    alert("error loading member database, please refresh your browser"); // If we got an error we handle it
+} 
+
 //functions to create(login) user and remove user...
 let createUser = (node) =>{
     node.innerHTML = `
@@ -48,7 +120,7 @@ let createUser = (node) =>{
     loggedUser.viewedNotices = false;
     catchSettings();
     activateNotifications();
-    
+    autocomplete(searchBox, userList);
 } 
 
 let resetUser = (node) => {
@@ -65,7 +137,7 @@ let resetUser = (node) => {
     `;
     restoreSettings();
     catchSettings();
-    // activateNotifications();
+    autocomplete(searchBox, userList);
 }
 
 let removeUser = (node) =>{
@@ -81,6 +153,7 @@ function supportsLocalStorage() {
     try {
         return 'localStorage' in window && window['localStorage'] !== null;
     } catch (e) {
+        alert('this page requires localstorage enabled');
         return false;
     }
 }
@@ -105,18 +178,17 @@ let createAlert = (parentElement, jabba) => {
 };
 
 createAlert(alertBox, 'Failed to load database server.  You are currently viewing the most recent data held by your local server');
-
-
-// Traffic line chart
-let trafficCanvas = document.getElementById('traffic-chart');
-let trafficChart = new Chart(trafficCanvas, {
+//default canvas.
+let TrafficCanvas = document.getElementById('traffic-chart')
+let trafficChart = new Chart(TrafficCanvas, {
     type: 'line',
     data: {
-        labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
         datasets: [{
-            data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+            data: [10000, 11400, 10700, 13050, 14500, 11750, 11250, 12850, 12250, 10500, 12500, 13400],
             backgroundColor: 'rgba(102, 102, 153, .5)',
             borderColor: '#669',
+            lineTension: 0,
         }],
     },
     options: {
@@ -133,9 +205,160 @@ let trafficChart = new Chart(trafficCanvas, {
         },
         legend: {
             display: false
-        }
+        },
+        responsive: true,
     }
 })
+
+trafficList.addEventListener('click', function(e){
+    let hrBtn = document.querySelector('#hrButton');
+    let dyBtn = document.querySelector('#dyButton');
+    let wkBtn = document.querySelector('#wkButton');
+    let mthBtn = document.querySelector('#mthButton');
+    switch (e.target){
+        case hrBtn:
+            hrBtn.classList.toggle('selected', true);
+            dyBtn.classList.toggle('selected', false);
+            wkBtn.classList.toggle('selected', false);
+            mthBtn.classList.toggle('selected', false);
+            trafficChart = new Chart(TrafficCanvas, {
+                type: 'line',
+                data: {
+                    labels: ['2AM', '4AM', '6AM', '8AM', '10AM', '12PM', '2PM', '4PM', '6PM', '8PM', '10PM', '12AM'],
+                    datasets: [{
+                        data: [2, 5, 7, 5, 9, 7, 10, 12, 10, 22, 10, 5],
+                        backgroundColor: 'rgba(102, 102, 153, .5)',
+                        borderColor: '#669',
+                        lineTension: 0,
+                    }],
+                },
+                options: {
+                    aspectRatio: 2.5,
+                    animation: {
+                        duration: 0
+                    },
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    },
+                    legend: {
+                        display: false
+                    },
+                    responsive: true,
+                }
+            })
+            break;
+        case dyBtn:
+            hrBtn.classList.toggle('selected', false);
+            dyBtn.classList.toggle('selected', true);
+            wkBtn.classList.toggle('selected', false);
+            mthBtn.classList.toggle('selected', false);
+            trafficChart = new Chart(TrafficCanvas, {
+                type: 'line',
+                data: {
+                    labels: ['S', 'M', 'T', 'W', 'Th', 'F', 'Sa'],
+                    datasets: [{
+                        data: [75, 100, 175, 125, 225, 210, 105],
+                        backgroundColor: 'rgba(102, 102, 153, .5)',
+                        borderColor: '#669',
+                        lineTension: 0,
+                    }],
+                },
+                options: {
+                    aspectRatio: 2.5,
+                    animation: {
+                        duration: 0
+                    },
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    },
+                    legend: {
+                        display: false
+                    },
+                    responsive: true,
+                }
+            });
+            break;
+        case wkBtn:
+            hrBtn.classList.toggle('selected', false);
+            dyBtn.classList.toggle('selected', false);
+            wkBtn.classList.toggle('selected', true);
+            mthBtn.classList.toggle('selected', false);
+            trafficChart = new Chart(TrafficCanvas, {
+                type: 'line',
+                data: {
+                    labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+                    datasets: [{
+                        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+                        backgroundColor: 'rgba(102, 102, 153, .5)',
+                        borderColor: '#669',
+                        lineTension: 0,
+                    }],
+                },
+                options: {
+                    aspectRatio: 2.5,
+                    animation: {
+                        duration: 0
+                    },
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    },
+                    legend: {
+                        display: false
+                    },
+                    responsive: true,
+                }
+            })
+            break;
+        case mthBtn:
+            hrBtn.classList.toggle('selected', false);
+            dyBtn.classList.toggle('selected', false);
+            wkBtn.classList.toggle('selected', false);
+            mthBtn.classList.toggle('selected', true);
+            trafficChart = new Chart(TrafficCanvas, {
+                type: 'line',
+                data: {
+                    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                    datasets: [{
+                        data: [10000, 11400, 10700, 13050, 14500, 11750, 11250, 12850, 12250, 10500, 12500, 13400],
+                        backgroundColor: 'rgba(102, 102, 153, .5)',
+                        borderColor: '#669',
+                        lineTension: 0,
+                    }],
+                },
+                options: {
+                    aspectRatio: 2.5,
+                    animation: {
+                        duration: 0
+                    },
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    },
+                    legend: {
+                        display: false
+                    },
+                    responsive: true,
+                }
+            })
+            break;
+        }
+});
+
 // Daily bar chart
 let dailyCanvas = document.getElementById('daily-traffic');
 let dailyChart = new Chart(dailyCanvas, {
@@ -146,6 +369,10 @@ let dailyChart = new Chart(dailyCanvas, {
             data: [75, 100, 175, 125, 225, 210, 105],
             backgroundColor: '#669',
             borderWidth: 1,
+            // barPercentage: 0.5,
+            // barThickness: 2,
+            // maxBarThickness: 4,
+            // categoryPercentage: 1.0,
         }],
     },
     options: {
@@ -158,7 +385,8 @@ let dailyChart = new Chart(dailyCanvas, {
         },
         legend: {
             display: false
-        }
+        },
+        responsive: true,
     }
 });
 // Mobile pie graph
@@ -185,69 +413,13 @@ let mobileGraph = new Chart(mobileCanvas, {
                 boxWidth: 20,
                 // fontStyle: 'bold'
             }
-        }
+        },
+        responsive: true,
     }
 });
 
-//members as per the mockup...
-const members = [
-    {
-        name: {
-            first: 'Victoria',
-            last: 'Chambers'},
-        email: 'victoria.chambers80@example.com',
-        spec: {
-            method: "commented on",
-            medium: "YourApp's SEO Tips",
-            time: "4 hours ago"
-        },
-        picture: {
-            thumbnail: "images/member-1.jpg"},
-        joindate: "09/30/2019"
-    },
-    {
-        name: {
-            first: 'Dale',
-            last: 'Byrd'},
-        email: 'dale.byrd52@example.com',
-        spec: {
-            method: "liked the post",
-            medium: "Facebook's changes for 2016",
-            time: "5 hours ago"},
-        picture: {
-            thumbnail: "images/member-2.jpg"
-        },
-        joindate: "11/05/2015"
-    },
-    {
-        name: {
-            first:'Dawn',
-            last: 'Wood'},
-        email: 'dawn.wood16@example.com',
-        spec: {
-            method: "commented on",
-            medium: "Facebook's changes for 2016",
-            time: "5 hours ago"},
-        picture: {
-            thumbnail: "images/member-3.jpg"
-        },
-        joindate: "01/26/1901"
-    },
-    {
-        name: {
-            first: 'Dan',
-            last: 'Oliver'}, 
-        email: 'dan.oliver52@example.com',
-        spec: {
-            method: "posted to",
-            medium: "YourApp's SEO Tips",
-            time: "1 day ago"},
-        picture: {
-            thumbnail: "images/member-4.jpg"
-        },
-        joindate: "04/25/1939"
-    }
-];
+
+
 
 let notices = [
     "You're great",
@@ -259,21 +431,23 @@ let notices = [
 ];
 
 function getRandomNotifications() {
-    let phrase = [];
-    let first = Math.floor(Math.random() * notices.length);
-    phrase = notices.splice(first, 1);
-    let second = Math.floor(Math.random() * notices.length);
-    phrase.push(notices.splice(second, 1));
+    function arrayShuffle(arr) {
+        let count = arr.length;
+        while (count) {
+            arr.push(arr.splice(Math.floor(Math.random() * count), 1)[0]);
+            count -= 1;
+        }
+    }
+    arrayShuffle(notices);
+    let phrase = notices.splice(0, 2);
     noticeDiv.className = 'notice-div';
-    noticeDiv.innerHTML = `
-                        <h4>${phrase[0]}</h4>
-                        <h4>${phrase[1]}</h4>`;
+    noticeDiv.innerHTML = "<h4>"+phrase[0]+"</h4><h4>"+phrase[1]+"</h4>"
     notification.appendChild(noticeDiv);
-    notices.push(phrase);
+    notices = notices.concat(phrase);
 }
 
 let activateNotifications = () => {
-    $(notification).on('click', function(){
+    $(notification).unbind().on('click', function(){
        if (notification.className == 'active'){
             getRandomNotifications();
             notification.className = "viewed";
@@ -281,32 +455,21 @@ let activateNotifications = () => {
             localStorage.currentUser = JSON.stringify(loggedUser);
         } else if (notification.className == "viewed") {
             let oldNotices = document.querySelector('.notice-div');
+            oldNotices.innerHTML = "";
             notification.removeChild(oldNotices);
             notification.className = "";
             localStorage.currentUser = JSON.stringify(loggedUser);
         }
-    })
-}
-
-
-let userList = members.concat(randomUsers); //adding the created list to the random list for extra search names
-if (typeof userList == null) {
-    alert("error loading member profiles from Server, please refresh your page!");
-} else {
-    for(let i = 0; i<userList.length; i++) {
-    let user = userList[i];
-    user.name.full = user.name.first + " " +user.name.last;
-    user.name.bridged = user.name.first + user.name.last;
-    }
-}
+    });
+};
 
 
 
 function createMembersList(parent){
     if(loggedUser.loggedIn){    
-        for (let i = 0; i < 4; i++) {
+        for (let i = 0; i < members.length; i++) {
             let newMember = document.createElement('li');
-            let member = userList[i];
+            let member = members[i];
             let text =
                 `<img src="${member.picture.thumbnail}" alt="member image"/>
                 <div>    
@@ -326,9 +489,9 @@ function createMembersList(parent){
 // Recent Activity List
 
 function createRecentActivityList(parent) {
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < members.length; i++) {
         let activityList = document.createElement('li');
-        let member = userList[i];
+        let member = members[i];
         let text =
             `<img src="${member.picture.thumbnail}" alt="member image"/>
             <div>    
@@ -423,7 +586,6 @@ function autocomplete(inp, arr) {
         closeAllLists(e.target);
     });
 } 
-autocomplete(searchBox, userList);
 
 sendButton.addEventListener('click', function(e){
     e.preventDefault();
@@ -463,7 +625,7 @@ sendButton.addEventListener('click', function(e){
         searchBox.value = "";
     }   
     function closeAllLists() {
-        popItems = document.getElementsByClassName("pop-up");
+        let popItems = document.getElementsByClassName("pop-up");
         for (let i = 0; i < popItems.length; i++) {
            popItems[i].parentNode.removeChild(popItems[i]);
         }
